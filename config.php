8f04b7e420f1db3d83bce0721f11ffa817485723
H4r1ey <85908279+H4r1eyDev@users.noreply.github.com>
2021-08-21 10:55:37 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,23 @@
+<?
+    // if this is not filled in correclty, some pages may fail to the load.
+    define('BASE_URL', 'https://h4r1ey.xyz');
+
+    // custom config. (Site Config.)
+    define('SHORT_SITE_NAME', 'H4r1ey');
+    define('SITE_NAME', 'Community Landing Page');
+    define('NORMAL_SITE_NAME', 'H4r1eyDevelopment');
+    define('FOOTER_COLOUR', '#000');
+	define('FOOTER_LETTERING_COLOUR', '#fff');
+
+    // this can be as large/small as you wish, the more words doesn't matter but will increase the amount of scrolling needed.
+    define('ABOUT_ME', 'Hello I am Harley, I am a website and software developer! This is my first ever PHP project! Want to find more of my Projects? Look no further, you can find them within my Discord
+    I sell different types of products within there! Mainly Discord bots, now learning and getting into PHP.');
+
+    // CONTACT AND INFORMATION
+    define('OWNER_NAME', 'H4r1ey');
+    define('OWNERS_DISCORD', 'Harley.#6969');
+    define('DISCORD_INVITE', 'https://discord.gg/43akmun7Qd');
+    define('MAIN_COLOUR', '#ff0000');
+	define('COPYRIGHT', '2021 H4r1ey Development');
+    define('CONTACT_URL', 'https://h4r1ey-dev.xyz');
+?>
