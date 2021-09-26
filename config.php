28fc4a19135d9641194acb0c550e8241fcacef5e
izMystic <mystic@izmystic.dev>
2021-09-26 09:51:39 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
     // if this is not filled in correclty, some pages may fail to the load.
     $BASE_URL = 'https://h4r1ey.xyz';
-
+    
     // custom config. (Site Config.)
     $SHORT_SITE_NAME = 'H4r1ey';
     $SITE_NAME = 'Community Landing Page';
     $NORMAL_SITE_NAME = 'H4r1eyDevelopment';
     $FOOTER_COLOUR = '#000';
-	$FOOTER_LETTERING_COLOUR = '#fff';
+    $FOOTER_LETTERING_COLOUR = '#fff';
 
     // this can be as large/small as you wish, the more words doesn't matter but will increase the amount of scrolling needed.
     $ABOUT_ME = 'Hello I am Harley, I am a website and software developer! This is my first ever PHP project! Want to find more of my Projects? Look no further, you can find them within my Discord
@@ -18,6 +18,6 @@
     $OWNERS_DISCORD = 'Harley.#6969';
     $DISCORD_INVITE = 'https://discord.gg/43akmun7Qd';
     $MAIN_COLOUR = '#ff0000';
-	$COPYRIGHT = '2021 H4r1ey Development';
+    $COPYRIGHT = '2021 H4r1ey Development';
     $CONTACT_URL = 'https://h4r1ey-dev.xyz';
 ?>
