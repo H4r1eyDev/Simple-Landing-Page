8e2de7885e272e50b6e61576a02120925b0f6a9e
izMystic <mystic@izmystic.dev>
2021-09-26 09:50:08 -0400

    fix for newer php version

--- config.php
+++ config.php
@@ -1,23 +1,23 @@
-<?
+<?php
     // if this is not filled in correclty, some pages may fail to the load.
-    define('BASE_URL', 'https://h4r1ey.xyz');
+    $BASE_URL = 'https://h4r1ey.xyz';
 
     // custom config. (Site Config.)
-    define('SHORT_SITE_NAME', 'H4r1ey');
-    define('SITE_NAME', 'Community Landing Page');
-    define('NORMAL_SITE_NAME', 'H4r1eyDevelopment');
-    define('FOOTER_COLOUR', '#000');
-	define('FOOTER_LETTERING_COLOUR', '#fff');
+    $SHORT_SITE_NAME = 'H4r1ey';
+    $SITE_NAME = 'Community Landing Page';
+    $NORMAL_SITE_NAME = 'H4r1eyDevelopment';
+    $FOOTER_COLOUR = '#000';
+	$FOOTER_LETTERING_COLOUR = '#fff';
 
     // this can be as large/small as you wish, the more words doesn't matter but will increase the amount of scrolling needed.
-    define('ABOUT_ME', 'Hello I am Harley, I am a website and software developer! This is my first ever PHP project! Want to find more of my Projects? Look no further, you can find them within my Discord
-    I sell different types of products within there! Mainly Discord bots, now learning and getting into PHP.');
+    $ABOUT_ME = 'Hello I am Harley, I am a website and software developer! This is my first ever PHP project! Want to find more of my Projects? Look no further, you can find them within my Discord
+    I sell different types of products within there! Mainly Discord bots, now learning and getting into PHP.';
 
     // CONTACT AND INFORMATION
-    define('OWNER_NAME', 'H4r1ey');
-    define('OWNERS_DISCORD', 'Harley.#6969');
-    define('DISCORD_INVITE', 'https://discord.gg/43akmun7Qd');
-    define('MAIN_COLOUR', '#ff0000');
-	define('COPYRIGHT', '2021 H4r1ey Development');
-    define('CONTACT_URL', 'https://h4r1ey-dev.xyz');
+    $OWNER_NAME = 'H4r1ey';
+    $OWNERS_DISCORD = 'Harley.#6969';
+    $DISCORD_INVITE = 'https://discord.gg/43akmun7Qd';
+    $MAIN_COLOUR = '#ff0000';
+	$COPYRIGHT = '2021 H4r1ey Development';
+    $CONTACT_URL = 'https://h4r1ey-dev.xyz';
 ?>
